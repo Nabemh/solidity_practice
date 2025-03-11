fde4beacf50bc322afce35e59c47aa8a9497369e
Nabemh <nhanior@gmail.com>
2025-03-11 14:01:52 +0100

    feat: multiple tweets

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -8,13 +8,17 @@ pragma solidity ^0.8.0;
 // Create a function to get tweet
 
 contract Twitter{
-    mapping(address => string) public tweets;
+    mapping(address => string[]) public tweets;
 
     function createTweet(string memory _tweet) public {
-        tweets[msg.sender] = _tweet;
+        tweets[msg.sender].push(_tweet);
     }
 
-    function getTweet(address _owner) public view returns (string memory){
+    function getTweet(address _owner, uint _i) public view returns (string memory){
+        return tweets[_owner][_i];
+    }
+
+    function getAllTweets(address _owner) public view returns (string[] memory){
         return tweets[_owner];
     }
-}
+} 
